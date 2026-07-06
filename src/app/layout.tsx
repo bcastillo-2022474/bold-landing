e1974a0f14f08b9e5ce0c110ad9b25607172f9a8
Rudy Queche <gabrielqueche048@gmail.com>
2026-07-05 18:26:53 -0600

    Update script for microsoft clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Space_Grotesk } from "next/font/google";
+import Script from "next/script";
 import { META, SITE, SOCIAL } from "@/constants/site";
 import "./globals.css";
 
@@ -69,6 +70,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xhy63q57r8");
+            `,
+          }}
+        />
+      </head>
       <body
         className={`${spaceGrotesk.variable} ${interFont.variable} antialiased`}
       >
